b89fbad4cb2c3b03c4116dff5348e40f116a2860
chrisbeardy <20585410+chrisbeardy@users.noreply.github.com>
2020-06-07 16:03:42 +0100

    number of tasks shown printout has been corrected

--- hiding.py
+++ hiding.py
@@ -6,12 +6,18 @@ filter incoming lines based on keywords (contexts/projects) we want to hide
 from subprocess import getstatusoutput
 import sys
 
+
 def main(todo_sh, to_hide):
     todos = getstatusoutput("{} ls".format(todo_sh))[1].split('\n')
+    todos.pop()
+    tasks = 0
     for line in todos:
         if all(keyword not in line for keyword in to_hide.split()):
             print(line)
+            tasks += 1
+    print("TODO: {} of {} tasks shown".format(tasks, len(todos) - 1))
     return True
+
 
 if __name__ == '__main__':
     status = not main(*sys.argv[1:])
